2c33260905342d29447cb8fd4aca728e76c12473
Abhishek Krishna Shrestha <abhishek.stha17@gmail.com>
2018-07-04 15:52:30 +0545

    connectionstring in appsetting.json and hotelsview and activitiesview models were created in dbviewmodel

---
+++ TravPense/TravPense/Models/DbViewModels/ActivitiesViews.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravPense.Models.DbViewModels
+{
+    public class ActivitiesViews
+    {
+        [Key]
+        public string ActivityId { get; set; }
+        public string ActivityName { get; set; }
+        public string LocationType { get; set; }
+        public string Duration { get; set; }
+        public Int64 MyProperty { get; set; }
+    }
+}
---
+++ TravPense/TravPense/Models/DbViewModels/HotelsView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravPense.Models.DbViewModels
+{
+    public class HotelsView
+    {
+        [Key]
+        public string HotelId { get; set; }
+        public string HotelName { get; set; }
+        public string Destination { get; set; }
+        public string Contact { get; set; }
+        public Int64 MinPrice { get; set; }
+        public Int64 MaxPrice { get; set; }
+    }
+}
